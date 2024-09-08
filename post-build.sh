fa44e7f45c5fa289e4697323377491ffe3d50b5d
Eric Rauer <eric@rauer.dev>
2024-09-08 15:23:15 -0700

    Build overlays for usb gadget and usb host

--- post-build.sh
+++ post-build.sh
@@ -9,5 +9,8 @@ mkdir -p $TARGET_DIR/usr/share/fwup
 #$HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-ops.conf -o $TARGET_DIR/usr/share/fwup/ops.fw
 #ln -sf ops.fw $TARGET_DIR/usr/share/fwup/revert.fw
 
+$HOST_DIR/bin/fdtoverlay -i $BINARIES_DIR/meson-g12a-superbird.dtb -o $BINARIES_DIR/meson-g12a-superbird-usb-gadget.dtb $BINARIES_DIR/watchdog.dtb
+$HOST_DIR/bin/fdtoverlay -i $BINARIES_DIR/meson-g12a-superbird-usb-gadget.dtb -o $BINARIES_DIR/meson-g12a-superbird-usb-host.dtb $BINARIES_DIR/usb-host.dtb
+
 # Copy the fwup includes to the images dir
 cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
